4bcd7218fe7f19b7a06a034397674c4e7858709e
Rubenchuquivilca <38230003+Rubenchuquivilca@users.noreply.github.com>
2020-06-18 13:44:39 -0500

    subir

--- proyecto/src/main/java/pe/edu/upeu/proyecto/entity/Categoria.java
+++ proyecto/src/main/java/pe/edu/upeu/proyecto/entity/Categoria.java
@@ -4,9 +4,12 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 @Entity
 @Table(name="categoria")
@@ -15,6 +18,7 @@ public class Categoria implements Serializable {
  
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
+
 
 private Long idcategoria;
 
--- proyecto/src/main/java/pe/edu/upeu/proyecto/entity/Producto.java
+++ proyecto/src/main/java/pe/edu/upeu/proyecto/entity/Producto.java
@@ -4,9 +4,12 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -22,37 +25,54 @@ private Long idproducto;
 private String nomprod;
 private double precio;
 private int cantidad;
-private Long idcategoria;
+
+
+@ManyToOne(fetch=FetchType.LAZY)
+
+@JoinColumn(name="idcategoria", nullable=false)
+ 
+private Categoria idcategoria;
+
 public Long getIdproducto() {
 	return idproducto;
 }
+
 public void setIdproducto(Long idproducto) {
 	this.idproducto = idproducto;
 }
+
 public String getNomprod() {
 	return nomprod;
 }
+
 public void setNomprod(String nomprod) {
 	this.nomprod = nomprod;
 }
+
 public double getPrecio() {
 	return precio;
 }
+
 public void setPrecio(double precio) {
 	this.precio = precio;
 }
+
 public int getCantidad() {
 	return cantidad;
 }
+
 public void setCantidad(int cantidad) {
 	this.cantidad = cantidad;
 }
-public Long getIdcategoria() {
+
+public Categoria getIdcategoria() {
 	return idcategoria;
 }
-public void setIdcategoria(Long idcategoria) {
+
+public void setIdcategoria(Categoria idcategoria) {
 	this.idcategoria = idcategoria;
 }
+
 
 
 }
